aa15d55a10429259948df32a31c10ac4170ed92b
Carl Banbury <carl@googolmail.co.uk>
2016-03-15 20:07:20 +0000

    working example using specktackle

--- client/home/home.js
+++ client/home/home.js
@@ -5,6 +5,56 @@ Template.home.onCreated(function (){
     this.samples.set([]);
 });
 
+Template.home.onRendered(function(){
+    var chart = st.chart          // new chart
+    .ir()                 // of type MS
+    .xlabel("Wave Number")        // x-axis label
+    .ylabel("Intensity") // y-axis label
+
+    chart.render("#stgraph");     // render chart to id 'stgraph'
+    var handle = st.data          // new handler
+    .set()                    // of type set
+    .ylimits([0, 1000])       // y-domain limits
+    .x("peaks.mz")            // x-accessor
+    .y("peaks.intensity");    // y-accessor
+    chart.load(handle);
+
+    // // // bind the data handler to the chart
+    handle.add(
+        {
+            "spectrumId": "PR100729",
+            "mzStart": 98.0598,
+            "mzStop": 160.8423,
+            "peaks": [
+                {
+                    "mz": 98.0598,
+                    "intensity": 222.0
+                },
+                {
+                    "mz": 116.0714,
+                    "intensity": 820.0
+                },
+                {
+                    "mz": 142.0505,
+                    "intensity": 668.0
+                },
+                {
+                    "mz": 160.061,
+                    "intensity": 999.0
+                },
+                {
+                    "mz": 160.8423,
+                    "intensity": 781.0
+                },
+                {
+                    "mz": 160.9,
+                    "intensity": 800
+                }
+            ]
+        }
+    )
+});
+
 Template.home.helpers({
     numSamples: function() {
         return Template.instance().samples.get().length;
