f3d2d6501f0f1b6802d68c1c2959e27f58ae2532
Carl Banbury <carl@googolmail.co.uk>
2016-03-15 17:53:17 +0000

    uploading files

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Simple tool for analysing raw data from Raman spectroscopy.
 
 ## Planned Features:
 - Drag and Drop file upload from `.txt` spectral data
-- Preview of all and mean data
-- Optional fluorescent background subtraction using least squares fitting
+- Preview of all and mean data ([SpecTackle](https://bitbucket.org/sbeisken/specktackle))
+- Optional fluorescent background subtraction using least squares fitting ([regression-js](https://github.com/Tom-Alexander/regression-js))
 - Peak fitting of individual specta
-- PCA analysis of data classes and automated grouping
+- PCA analysis of data classes and automated grouping ([ml-pca](https://www.npmjs.com/package/ml-pca))
--- client/home/home.js
+++ client/home/home.js
@@ -1,3 +1,40 @@
-Template.home.onRendered(function() {
-    $('.modal-trigger').leanModal();
+Template.home.onCreated(function (){
+    this.samples = new ReactiveVar;
+    this.waveNumber = new ReactiveVar;
+    this.waveNumber.set([]);
+    this.samples.set([]);
+});
+
+Template.home.helpers({
+    numSamples: function() {
+        return Template.instance().samples.get().length;
+    }
+});
+
+Template.home.events({
+    "change input[type='file']": function(event) {
+        var files = event.currentTarget.files;
+
+        var template = Template.instance();
+        var waveNumber = [];
+        var sample = [];
+
+        for (var i = 0; i < files.length; i++) {
+            var reader = new FileReader();
+            reader.onload = function(progressEvent){
+                // By lines
+                var lines = this.result.split('\n');
+                thisSample = [];
+                for(var line = 0; line < lines.length; line++) {
+                    data = lines[line].split('\u0009');
+                    waveNumber[line] = data[0];
+                    thisSample[line] = data[1];
+                }
+
+                template.waveNumber.set(waveNumber);
+                template.samples.set(sample);
+            };
+            reader.readAsText(files[i]);
+        }
+    }
 });
