75df6fff87e59ddf8c1c81fa1cdf8d466587da4c
Carl Banbury <carl@googolmail.co.uk>
2016-05-02 15:19:20 +0100

    sorting out sidebar

--- client/navbar/menu.js
+++ client/navbar/menu.js
@@ -1,9 +1,3 @@
-Template.navbar_menu.events({
-    'click .nav-home': function() {
-        FlowRouter.go('home');
-    }
-});
-
 Template.navbar_menu.helpers({
     activeIfNameIs: function(name) {
         if (FlowRouter.getRouteName() === name) {
