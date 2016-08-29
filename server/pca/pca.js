8d9276a948ef5fd6199a8a9f5bdc9f9ff89f3c6f
Carl Banbury <carl@banbury.ch>
2016-08-29 07:53:07 +0100

    adding ml-pca package and dummy, currently broken pca page

--- server/pca/pca.js
+++ server/pca/pca.js
@@ -1,31 +1,37 @@
 Meteor.methods({
     'pca:get': function(classes) {
-        var data = [];
-        var out = {};
-        spectra = Spectra.find({tag: {$in: classes}}).fetch();
+        import pca from 'ml-pca';
 
-        if (spectra.length > 0) {
-            spectra.forEach(function(spectrum) {
-                data.push(spectrum.y);
-            });
+        var foo = new pca([[]]);
 
-            var sylvester = Meteor.npmRequire('sylvester');
-            var a = $M(data).transpose();
+        console.log(foo);
 
-            var pca = a.pcaProject(2);
-
-            scores = pca.U.elements;
-
-            for (i=1; i<spectra.length; i++) {
-                var row = scores[i];
-                if (!out[spectra[i].tag]) {
-                    out[spectra[i].tag] = {x: [], y: []};
-                }
-
-                out[spectra[i].tag].x.push(row[0]);
-                out[spectra[i].tag].y.push(row[1]);
-            }
-        }
-        return out;
+        // var data = [];
+        // var out = {};
+        // spectra = Spectra.find({tag: {$in: classes}}).fetch();
+        //
+        // if (spectra.length > 0) {
+        //     spectra.forEach(function(spectrum) {
+        //         data.push(spectrum.y);
+        //     });
+        //
+        //     var sylvester = Meteor.npmRequire('sylvester');
+        //     var a = $M(data).transpose();
+        //
+        //     var pca = a.pcaProject(2);
+        //
+        //     scores = pca.U.elements;
+        //
+        //     for (i=1; i<spectra.length; i++) {
+        //         var row = scores[i];
+        //         if (!out[spectra[i].tag]) {
+        //             out[spectra[i].tag] = {x: [], y: []};
+        //         }
+        //
+        //         out[spectra[i].tag].x.push(row[0]);
+        //         out[spectra[i].tag].y.push(row[1]);
+        //     }
+        // }
+        // return out;
     }
 });
