33c1e0cd6f4fc558aa28e915a27d17c79e93461c
Carl Banbury <carl@banbury.ch>
2016-09-03 04:04:57 +0100

    pagination for displaying spectra

--- client/my-spectra/mySpectra.js
+++ client/my-spectra/mySpectra.js
@@ -1,7 +1,3 @@
-Template.mySpectra.onCreated(function() {
-    Meteor.subscribe('spectra');
-});
-
 Template.mySpectra.helpers({
     spectra: function() {
         return Spectra.find({uid: Meteor.userId()});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,10 @@
-Meteor.publish("spectra", function () {
-    return Spectra.find({uid: this.userId});
+Meteor.publish("spectra", function (skip, limit) {
+    Counts.publish(this, 'total_spectra', Spectra.find());
+    if (skip < 0) {
+        skip = 0;
+    }
+    return Spectra.find({uid: this.userId}, {
+        skip: skip,
+        limit: 10
+    });
 });
