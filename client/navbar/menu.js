a92cfad15ba62f401788b29eaf1d3629a822c5e9
Carl Banbury <carl@banbury.ch>
2016-09-03 05:23:15 +0100

    logout button and links from home page

--- client/navbar/menu.js
+++ client/navbar/menu.js
@@ -5,3 +5,16 @@ Template.navbar_menu.helpers({
         }
     }
 });
+
+Template.navbar_menu.rendered = function() {
+    $(".dropdown-button").dropdown({
+        hover: false
+    });
+};
+
+Template.navbar_menu.events({
+    'click .logout': function() {
+        AccountsTemplates.logout();
+        FlowRouter.go('home');
+    }
+})
