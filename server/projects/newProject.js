a445faff1993e4ff2713eaf8884371baa0961f09
Carl Banbury <carl@banbury.ch>
2018-09-14 02:13:28 -0700

    removing spectra on deletion of project or labels

--- server/projects/newProject.js
+++ server/projects/newProject.js
@@ -4,10 +4,20 @@ Meteor.methods({
 		Projects.insert(data);
 	},
 	'projects:update': function(data) {
+		var old = Projects.findOne({_id: data._id, uid: Meteor.userId()}).labels;
+		old.forEach((item)=>{
+			var match = data.labels.filter((label)=>{return label.tag === item.tag});
+			if (match.length < 1) {
+				Spectra.remove({projectId: data._id, uid: Meteor.userId(), label: item.tag});
+			}
+		})
+
 		data.uid = Meteor.userId();
 		Projects.update({_id: data._id}, {$set: data});
 	},
 	'projects:remove': function(id) {
+		Spectra.remove({projectId: id, uid: Meteor.userId()})
+
 		return Projects.remove({
 		    uid: Meteor.userId(),
 		    _id: id
